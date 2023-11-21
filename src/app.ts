828ab75ec52eb3aface50dc37470d4c67215aeba
Md-Mobassher <you@example.com>
2023-11-21 11:30:11 +0600

    Basic project setup with typescript express mongoose

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+
+const app: Application = express()
+
+app.use(express.json())
+app.use(cors())
+
+// app.use('/api/v1/users', userRoutes)
+
+
+app.get('/', (req: Request, res: Response) => {
+  res.status(200).json({
+    status: 'success',
+    message: 'Welcome to new server',
+  })
+})
+
+export default app
