72b82564d6fbee6ef5b3a302d24483381cb00677
Md Mobassher Hossain <mobassherpautex@gmail.com>
2024-05-12 16:07:53 +0600

    readme file updated

---
+++ README.md
@@ -0,0 +1,60 @@
+# Server Name
+
+## Deployment
+
+- **Live Link**: [Live server link]()
+
+## Postman API Documentation:
+
+- **[Postman API Documentation]()**
+
+## Technologies Used
+
+- TypeScript
+- Express
+- Mongoose
+- MongoDB
+
+## Getting Started
+
+To get a local copy of the project up and running, follow these steps:
+
+1. **Clone the Repository:**
+
+```shell
+git clone <repository-link>
+```
+
+2. **Navigate to the Project Directory:**
+
+```shell
+cd <project_name>
+```
+
+3. Please update the filename from `.env.example` to `.env`
+
+4. **Install Dependencies:**
+
+```shell
+yarn install
+```
+
+5. **Start the Server:**
+
+```shell
+yarn dev
+```
+
+The server will be running at **`http://localhost:5000`** .
+
+## Contributing
+
+Contributions are welcome! If you find any bugs or want to suggest improvements, please open an issue or create a pull request.
+
+## License
+
+MIT License
+
+## More Projects and Information
+
+👉 Explore additional projects and find out more about my work on my portfolio website: [Md Mobassher Hossain](https://dev-mobassher.web.app)
--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -6,17 +6,26 @@ dotenv.config({ path: path.join((process.cwd(), '.env')) })
 export default {
   NODE_ENV: process.env.NODE_ENV,
   port: process.env.PORT,
+
   database_url: process.env.DATABASE_URL,
   database_url_local: process.env.DATABASE_URL_LOCAL,
+
   bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
   default_password: process.env.DEFAULT_PASS,
   super_admin_password: process.env.SUPER_ADMIN_PASS,
-  jwt_access_secret: process.env.JWT_ACCESS_SECRET,
-  jwt_refresh_secret: process.env.JWT_REFRESH_SECRET,
-  jwt_access_expires_in: process.env.JWT_ACCESS_EXPIRES_IN,
-  jwt_refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN,
+
+  jwt: {
+    access_secret: process.env.JWT_ACCESS_SECRET,
+    refresh_secret: process.env.JWT_REFRESH_SECRET,
+    access_expires_in: process.env.JWT_ACCESS_EXPIRES_IN,
+    refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN,
+  },
+
+  cloudinary: {
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+  },
+
   reset_pass_ui_link: process.env.RESET_PASS_UI_LINK,
-  cloudinary_cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  cloudinary_api_key: process.env.CLOUDINARY_API_KEY,
-  cloudinary_api_secret: process.env.CLOUDINARY_API_SECRET,
 }
